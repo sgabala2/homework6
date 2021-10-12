aee68a0a23b47599a41c97085f4e8d0dc2a72d62
Shaina Gabala <sgabala2@jh.edu>
2021-10-11 21:07:22 -0400

    working on task 1. Almost complete with parts 1 and 2, working on part 3

--- hw6_starter/src/main/resources/public/js/employers.js
+++ hw6_starter/src/main/resources/public/js/employers.js
@@ -5,8 +5,19 @@ function deleteEmployer(employerName) {
     ).then(res => window.location.reload = window.location.reload(true));
 }
 
+function addNewEmployer(employerName) {
+    fetch('http://localhost:7000/employers?name=' + employerName, {
+            method: 'Post',
+        }
+    ).then(res => window.location.reload = window.location.reload(true));
+}
+
 let delButtons = document.querySelectorAll("li > button")
 Array.prototype.forEach.call(delButtons, function(button) {
     button.addEventListener('click', deleteEmployer.bind(null, button.id));
+});
+
+Array.prototype.forEach.call(delButtons, function(button) {
+    button.addEventListener('click', addNewEmployer.bind(null, button.id));
 });
 
