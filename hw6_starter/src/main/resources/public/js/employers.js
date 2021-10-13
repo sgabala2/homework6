7f506bac37e27b49dcea99bda8d7400ade496bca
Ayo Ajayi <ayomide.ajayi2839@gmail.com>
2021-10-13 19:23:45 -0400

    Finished Task 1 completely

--- hw6_starter/src/main/resources/public/js/employers.js
+++ hw6_starter/src/main/resources/public/js/employers.js
@@ -1,59 +1,42 @@
-// import {validateEmployer} from "./validate";
-// let val = validateEmployer();
-// window.alert(val);
-
-let data = {
-    name: document.getElementById("employerName").value,
-    sector: document.getElementById("employerName").value,
-    summary:document.getElementById("summary").value
-}
-
 function deleteEmployer(employerName) {
     fetch('http://localhost:7000/employers?name=' + employerName, {
             method: 'Delete',
         }
     ).then(res => window.location.reload = window.location.reload(true));
+
+
+}
+
+function validateEmployer(name, sector){
+    if ((name.length < 2 || name.length > 150) || (name.includes('$') || name.includes('@') || name.includes('^') || name.includes('%') || name.includes('~'))) {
+        alert("Employer name cannot be less than 2 characters, more than 150 characters, or include $, @, ^, %, ~!");
+        return false
+    }
+    if ((sector.length < 2 || sector.length > 100) ||
+        (sector.includes('$') || sector.includes('@') || sector.includes('^') || sector.includes('%') || sector.includes('~')) ||
+        (/\d/.test(sector))){
+        alert("Employer sector cannot be less than 2 characters, more than 100 characters, or include any digits or $, @, ^, %, ~!");
+        return false
+    }
+    return true
 }
 
 
 function addNewEmployer() {
-    console.log("in here");
-
-    var name = document.getElementById("employerName").value;
-    var sector = document.getElementById("employerName").value;
-    var summary = document.getElementById("summary").value;
-    // alert(name + summary + sector);
-
-    fetch('http://localhost:7000/employers?name=' + name + "&sector=" + sector + "&summary=" + summary,{
-            method: 'POST',
-        }
-    ).then(response => response.json())
-        .then(res => window.location.reload = window.location.reload(true));
-        // .then(res => window.location.reload = window.location.reload(true));
+    const name = document.getElementById("employerName").value;
+    const sector = document.getElementById("sector").value;
+    const summary = document.getElementById("summary").value;
+    if (validateEmployer(name, sector)){
+        fetch('http://localhost:7000/employers?name=' + name + "&sector=" + sector + "&summary=" + summary,{
+                method: 'POST',
+            }
+        ).then(res => window.location.reload = window.location.reload(true));
+    }
 }
-
-
-
-
-
-
-
-    //if(val) {
-    //     fetch('http://localhost:7000/employers?name=' + employerName + "&sector=" + employerSector + "&summary=" + employerSummary, {
-    //             method: 'Post',
-    //             //body: employerName, employerSector, employerSummary
-    //         }
-    //     ).then(res => window.location.reload = window.location.reload(true));
-    //}
-
 
 let delButtons = document.querySelectorAll("li > button")
 Array.prototype.forEach.call(delButtons, function(button) {
     button.addEventListener('click', deleteEmployer.bind(null, button.id));
 });
 
-
-// Array.prototype.forEach.call(delButtons, function(button) {
-//     button.addEventListener('click', addNewEmployer.bind(null, button.id));
-// });
 
